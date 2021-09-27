e0d5b4dc64fc68059202f2a6e74d556506ff8860
dungbik <q1w2e30630@gmail.com>
2021-09-27 17:32:55 +0900

    Feat: 분야 별 베스트에서 서적 데이터 수집 자동화 구현

--- crawl/crawl_book.py
+++ crawl/crawl_book.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver import ActionChains
+import math
 
 def getIndex(driver, content):
     elements = driver.find_elements_by_class_name("Ere_prod_mconts_LS")
@@ -16,35 +17,53 @@ driver = webdriver.Chrome(chromedriver)
 driver.implicitly_wait(1)
 action = ActionChains(driver)
 
+total = 10
+cidList = [1]
+totalPage = math.ceil(total / 25)
+
 start = time.time()
 
-for id in [279336165]:
-    driver.get('https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=' + str(id))
+for cid in cidList:
+    for page in range(1, totalPage + 1):
+        url = f'https://www.aladin.co.kr/shop/wbrowse.aspx?BrowseTarget=List&ViewRowsCount=25&ViewType=Detail&PublishMonth=0&SortOrder=2&page={page}&Stockstatus=1&PublishDay=84&CID={cid}'
+        driver.get(url)
 
-    title = driver.find_element_by_class_name("Ere_bo_title").text
+        for bookIdx in range(25):
 
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2)")
+            if total <= 0:
+                break
+            total -= 1
 
-    try:
-        # 목차에 더보기 버튼이 있을 때
-        hasTOCMore = driver.find_element_by_id("div_TOC_All")
+            book = driver.find_element_by_xpath(
+                f'//div[{bookIdx + 1}][@class="ss_book_box"]//*[@class="ss_book_list"]//li/a')
+            book.click()
 
-        driver.execute_script("javascript:fn_show_introduce_TOC('TOC')")
-    except:
-        pass
+            title = driver.find_element_by_class_name("Ere_bo_title").text
 
-    time.sleep(0.3)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2)")
 
-    elements = driver.find_elements_by_class_name("Ere_prod_mconts_R")
-    introIndex = getIndex(driver, "책소개")
-    contentsIndex = getIndex(driver, "목차")
+            try:
+                # 목차에 더보기 버튼이 있을 때
+                hasTOCMore = driver.find_element_by_id("div_TOC_All")
 
-    intro = elements[introIndex].text.replace("\n", " ")
-    contents = elements[contentsIndex].text.replace("\n", " ")
+                driver.execute_script("javascript:fn_show_introduce_TOC('TOC')")
+            except:
+                pass
 
-    print(title)
-    print(intro)
-    print(contents)
+            time.sleep(0.3)
+
+            elements = driver.find_elements_by_class_name("Ere_prod_mconts_R")
+            introIndex = getIndex(driver, "책소개")
+            contentsIndex = getIndex(driver, "목차")
+
+            intro = elements[introIndex].text.replace("\n", " ")
+            contents = elements[contentsIndex].text.replace("\n", " ")
+
+            print(title)
+            print(intro)
+            print(contents)
+
+            driver.back()
 
 driver.close()
 
