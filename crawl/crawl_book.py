e1d5a3f2804bc8c460ba19c59ff6a2287d256690
dungbik <q1w2e30630@gmail.com>
2021-09-27 17:19:31 +0900

    Fix: 유동적으로 소개/목차 index 처리

--- crawl/crawl_book.py
+++ crawl/crawl_book.py
@@ -2,6 +2,15 @@ from selenium import webdriver
 import time
 from selenium.webdriver import ActionChains
 
+def getIndex(driver, content):
+    elements = driver.find_elements_by_class_name("Ere_prod_mconts_LS")
+    index = 0
+    for element in elements:
+        if element.text == content:
+            return index
+        index += 1
+    return -1
+
 chromedriver = 'C:/Temp/chromedriver.exe'
 driver = webdriver.Chrome(chromedriver)
 driver.implicitly_wait(1)
@@ -19,8 +28,11 @@ for id in [279336165]:
     time.sleep(0.3)
 
     elements = driver.find_elements_by_class_name("Ere_prod_mconts_R")
-    intro = elements[5].text.replace("\n", " ")
-    contents = elements[6].text.replace("\n", " ")
+    introIndex = getIndex(driver, "책소개")
+    contentsIndex = getIndex(driver, "목차")
+
+    intro = elements[introIndex].text.replace("\n", " ")
+    contents = elements[contentsIndex].text.replace("\n", " ")
 
     print(title)
     print(intro)
