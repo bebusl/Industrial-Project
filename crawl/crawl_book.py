4b001da9f88aeaa562dc568a498e49cc3122dc6d
dungbik <q1w2e30630@gmail.com>
2021-09-27 17:41:12 +0900

    Feat: 데이터 수집 재시도 기능 구현

--- crawl/crawl_book.py
+++ crawl/crawl_book.py
@@ -13,6 +13,53 @@ def getIndex(driver, content):
         index += 1
     return -1
 
+def getBook(driver, bookIdx, data):
+    book = driver.find_element_by_xpath(
+        f'//div[{bookIdx + 1}][@class="ss_book_box"]//*[@class="ss_book_list"]//li/a')
+    book.click()
+
+    title = driver.find_element_by_class_name("Ere_bo_title").text
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2)")
+
+    try:
+        # 목차에 더보기 버튼이 있을 때
+        hasTOCMore = driver.find_element_by_id("div_TOC_All")
+
+        driver.execute_script("javascript:fn_show_introduce_TOC('TOC')")
+    except:
+        pass
+
+    time.sleep(0.3)
+
+    elements = driver.find_elements_by_class_name("Ere_prod_mconts_R")
+    introIndex = getIndex(driver, "책소개")
+    contentsIndex = getIndex(driver, "목차")
+
+    intro = elements[introIndex].text.replace("\n", " ")
+    contents = elements[contentsIndex].text.replace("\n", " ")
+
+    if introIndex > 0 and contentsIndex == -1:
+        print(title, "목차 가져오기 실패")
+        data['title'].append(title)
+        data['intro'].append(intro)
+        data['contents'].append("")
+    elif introIndex == -1 and contentsIndex > 0:
+        print(title, "소개 가져오기 실패")
+        data['title'].append(title)
+        data['intro'].append("")
+        data['contents'].append(contents)
+    elif introIndex == -1 and contentsIndex == -1:
+        print(title, "소개/목차 가져오기 실패")
+        return False
+    else:
+        data['title'].append(title)
+        data['intro'].append(intro)
+        data['contents'].append(contents)
+
+    driver.back()
+    return True
+
 chromedriver = 'C:/Temp/chromedriver.exe'
 driver = webdriver.Chrome(chromedriver)
 driver.implicitly_wait(1)
@@ -35,51 +82,18 @@ for cid in cidList:
 
             if total <= 0:
                 break
+
             total -= 1
 
-            book = driver.find_element_by_xpath(
-                f'//div[{bookIdx + 1}][@class="ss_book_box"]//*[@class="ss_book_list"]//li/a')
-            book.click()
+            retryCount = 5
+            while retryCount > 0:
+                if getBook(driver, bookIdx, data):
+                    break
+                retryCount -= 1
+                print("재시도", retryCount, page, bookIdx)
 
-            title = driver.find_element_by_class_name("Ere_bo_title").text
-
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2)")
-
-            try:
-                # 목차에 더보기 버튼이 있을 때
-                hasTOCMore = driver.find_element_by_id("div_TOC_All")
-
-                driver.execute_script("javascript:fn_show_introduce_TOC('TOC')")
-            except:
-                pass
-
-            time.sleep(0.3)
-
-            elements = driver.find_elements_by_class_name("Ere_prod_mconts_R")
-            introIndex = getIndex(driver, "책소개")
-            contentsIndex = getIndex(driver, "목차")
-
-            intro = elements[introIndex].text.replace("\n", " ")
-            contents = elements[contentsIndex].text.replace("\n", " ")
-
-            if introIndex > 0 and contentsIndex == -1:
-                print(title, "목차 가져오기 실패")
-                data['title'].append(title)
-                data['intro'].append(intro)
-                data['contents'].append("")
-            elif introIndex == -1 and contentsIndex > 0:
-                print(title, "소개 가져오기 실패")
-                data['title'].append(title)
-                data['intro'].append("")
-                data['contents'].append(contents)
-            elif introIndex == -1 and contentsIndex == -1:
-                print(title, "소개/목차 가져오기 실패")
-            else:
-                data['title'].append(title)
-                data['intro'].append(intro)
-                data['contents'].append(contents)
-
-            driver.back()
+            if retryCount == 0:
+                print("수집 실패", page, bookIdx)
 
 driver.close()
 
