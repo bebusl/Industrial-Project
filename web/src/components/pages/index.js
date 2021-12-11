31f53e7bec3f7d2082ad97550e0f10f26283150d
bebusl <bebus1998@naver.com>
2021-12-11 23:11:44 +0900

    Feat: 리액트 페이지 라우트

---
+++ web/src/components/pages/index.js
@@ -0,0 +1,6 @@
+import Main from "./Main";
+import Keyword from "./Keyword";
+import Mypage from "./Mypage";
+import Ranking from "./Ranking";
+import Book from "./Book";
+export { Main, Keyword, Mypage, Ranking, Book };
