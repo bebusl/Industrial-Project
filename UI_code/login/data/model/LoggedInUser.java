4a5444e0f942c1a07269a68cccacaa52d9e981fd
leejeongeun0 <75784140+leejeongeun0@users.noreply.github.com>
2021-12-11 16:29:49 +0900

    Update LoggedInUser.java

--- UI_code/login/data/model/LoggedInUser.java
+++ UI_code/login/data/model/LoggedInUser.java
@@ -1,8 +1,5 @@
 package com.example.push_book.data.model;
 
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
 public class LoggedInUser {
 
     private String userId;
